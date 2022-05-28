e3856e6407e6a7295c54f85fb0f97249b0f489ee
xztaityozx <taityonohibi@gmail.com>
2022-05-28 21:05:05 +0900

    fix: 引数の書き方難しい

--- entry-point.sh
+++ entry-point.sh
@@ -6,8 +6,8 @@ shift
 RESULT="$(/owari ${1+"@"})"
 
 if [[ "$AS_STEP_SUMMARY" == "true" ]]; then
-  /owari ${1+"@"} >> $GITHUB_STEP_SUMMARY
+  /owari "$@" >> $GITHUB_STEP_SUMMARY
 else
-  /owari ${1+"@"}
+  /owari "$@"
 end
 
