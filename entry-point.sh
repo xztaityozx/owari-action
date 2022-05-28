c0509d7882e5441db2522c4356826d2290bf9ac6
xztaityozx <taityonohibi@gmail.com>
2022-05-28 22:57:32 +0900

    feat: outputにowariの出力を含めるようにした

--- entry-point.sh
+++ entry-point.sh
@@ -3,8 +3,16 @@
 AS_STEP_SUMMARY="$1"
 shift
 
+TMP_FILE=/tmp/owari.$RANDOM
+eval /owari "$@" > $TMP_FILE
+
 if [[ "$AS_STEP_SUMMARY" == "true" ]]; then
-  (echo '```'; eval /owari -E=false "$@"; echo '```') >> $GITHUB_STEP_SUMMARY
+  (echo '```'; cat $TMP_FILE echo '```') >> $GITHUB_STEP_SUMMARY
 else
-  eval /owari "$@"
+  cat $TMP_FILE
+fi
+
+if [[ -z "GITHUB_ACTIONS" ]]; then
+  OUTPUT=$(cat $TMP_FILE)
+  echo "::set-output name=content::${OUTPUT//$'\n'/\\n}"
 fi
